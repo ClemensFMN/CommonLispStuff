1177b2a2dba5f9ddda579c97d2febeac564ff8bf
Clemens Novak <clemens@familie-novak.net>
2018-09-09 22:03:01 +0200

    some sudoku stuff

--- vector.lisp
+++ vector.lisp
@@ -11,3 +11,20 @@
 
 (get-board-pos 1 2)
 
+
+(defun uneq (x y)
+  (not (= x y)))
+
+(uneq 2 3)
+
+(defun coords-row-neighbours (row col)
+  (loop for i from 0 to 9 when (uneq i col) collect (list row i)))
+
+(coords-row-neighbours 2 4)
+
+
+(defun coords-col-neighbours (row col)
+  (loop for i from 0 to 9 when (uneq i row) collect (list i col)))
+
+(coords-col-neighbours 2 4)
+
